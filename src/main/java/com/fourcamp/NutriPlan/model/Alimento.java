f7090ed318cb9291f7544c1f6fb52678fef14918
Gbalestrieri9 <gabrielbalestrieri@hotmail.com>
2024-08-07 17:13:41 -0300

    Id ignore visualizar alimentos

--- src/main/java/com/fourcamp/NutriPlan/model/Alimento.java
+++ src/main/java/com/fourcamp/NutriPlan/model/Alimento.java
@@ -1,5 +1,6 @@
 package com.fourcamp.NutriPlan.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -9,6 +10,7 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Alimento {
 
     @Id
